ed3e7c6cb491510266c0f47032289aa19bc38f16
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2019-06-19 12:44:25 +0100

    Update and rename phenotypes.R to inputfiles.R

--- resources/checks/inputfiles.R
+++ resources/checks/inputfiles.R
@@ -6,7 +6,8 @@ suppressMessages(library(matrixStats))
 
 args <- (commandArgs(TRUE));
 phenotype_file <- as.character(args[1]);
-genotype_ids <- as.character(args[2])
+cov_file <- as.character(args[2]);
+genotype_ids <- as.character(args[3])
 
 message("Checking phenotypes: ", phenotype_file)
 
@@ -21,41 +22,15 @@ if(phenotype_file == "NULL")
 ph <- fread(phenotype_file)
 p1 <- dim(ph)[1]
 p2 <- dim(ph)[2]
+cov <-fread(cov_file)
 g_ids <- fread(genotype_ids.fam)
+
+#Check phenotype file
 
 if(names(ph)[1] !="IID")
 	{
 	msg <- paste0("First column in phenotype file should be the sample identified with the name IID")
 	errorlist <-c(errorlist, msg)
-	warning("ERROR: ", msg)
-	}
-
-commonids_pg<-Reduce(intersect, list(ph$IID, g_ids$V2))
-message(length(commonids_pg), " in common between phenotype and genotype data")
-
-if(length(commonids_mpc)<50)
-	{
-	msg <-paste0("fewer than 50 subjects with phenotype and genotype data")
-	warninglist<-c(warninglist, msg)
-	warning("Warning: ", msg)
-	}
-
-ph<-subset(ph, IID%in%commonids_pg)
-g_ids<-subset(g_ids, V2%in%commonids_pg)
-
-sex <- names(ph)[-1]names(ph)[-1] %in% c("Sex")]
-if(length(sex)<1)
-	{
-	msg <-paste0("Sex is not present in the phenotype file. Please check that the columns are labelled correctly")
-	errorlist <-c(errorlist,msg)
-	warning("ERROR: ", msg)
-	}
-
-age <- names(ph)[-1]names(ph)[-1] %in% c("Age")]
-if(length(age)<1)
-	{
-	msg <-paste0("Age is not present in the phenotype file. Please check that the columns are labelled correctly")
-	errorlist <-c(errorlist,msg)
 	warning("ERROR: ", msg)
 	}
 
@@ -66,6 +41,56 @@ if(length(nom)<1)
 	errorlist <-c(errorlist,msg)
 	warning("ERROR: ", msg)
 	}
+
+#Check covariate file
+
+if(names(cov)[1] !="IID")
+	{
+	msg <- paste0("First column in covariate file should be the sample identified with the name IID")
+	errorlist <-c(errorlist, msg)
+	warning("ERROR: ", msg)
+	}
+
+sex <- names(cov)[-1]names(cov)[-1] %in% c("Sex")]
+if(length(sex)<1)
+	{
+	msg <-paste0("Sex is not present in the covariate file. Please check that the columns are labelled correctly")
+	errorlist <-c(errorlist,msg)
+	warning("ERROR: ", msg)
+	}
+
+age <- names(cov)[-1]names(cov)[-1] %in% c("Age")]
+if(length(age)<1)
+	{
+	msg <-paste0("Age is not present in the covariate file. Please check that the columns are labelled correctly")
+	errorlist <-c(errorlist,msg)
+	warning("ERROR: ", msg)
+	}
+
+#Check genotype file
+
+if(names(g_ids)[2] !="V2")
+	{
+	msg <- paste0("The .fam file should not have a header.")
+	errorlist <-c(errorlist, msg)
+	warning("ERROR: ", msg)
+	}
+
+#Intersection of files
+commonids_pg<-Reduce(intersect, list(ph$IID, cov$IID, g_ids$V2))
+message(length(commonids_pg), " in common between phenotype, covariate and genotype data")
+
+if(length(commonids_mpc)<50)
+	{
+	msg <-paste0("fewer than 50 subjects with phenotype, covariate and genotype data")
+	warninglist<-c(warninglist, msg)
+	warning("Warning: ", msg)
+	}
+
+ph<-subset(ph, IID%in%commonids_pg)
+g_ids<-subset(g_ids, V2%in%commonids_pg)
+	
+#Phenotype checks
 
 if("Height" %in% nom)
 {
