3bca9a4ed680a54d4d63e4a109f8d8327c567bb7
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2019-06-19 12:05:53 +0100

    Update phenotypes.R

--- resources/checks/phenotypes.R
+++ resources/checks/phenotypes.R
@@ -4,8 +4,16 @@ warninglist <- list()
 library(data.table)
 suppressMessages(library(matrixStats))
 
-
-
 args <- (commandArgs(TRUE));
-phenotypes_file <- as.character(args[1]);
+phenotype_file <- as.character(args[1]);
 covariates_file <- as.character(args[2]);
+
+message("Checking phenotypes: ", phenotype_file)
+
+if(phenotype_file == "NULL") 
+{
+	msg <- paste0("No phenotype file has been provided.")
+	warninglist <- c(warninglist, msg)
+	message("WARNING: ", msg)
+	q()
+}
