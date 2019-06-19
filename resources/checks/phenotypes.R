01677088db9207130f605dd7f9f8421625bd5817
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2019-06-19 12:02:53 +0100

    Update phenotypes.R

--- resources/checks/phenotypes.R
+++ resources/checks/phenotypes.R
@@ -1,25 +1,11 @@
 errorlist <- list()
-
 warninglist <- list()
 
-
-
-
-
 library(data.table)
-
 suppressMessages(library(matrixStats))
 
 
 
 args <- (commandArgs(TRUE));
-
 phenotypes_file <- as.character(args[1]);
-
-meth_ids_file <- as.character(args[2]);
-
-covariates_file <- as.character(args[3]);
-
-cohort_descriptives_file <- as.character(args[4])
-
-ewas_phenotype_list_file <- as.character(args[5])
+covariates_file <- as.character(args[2]);
