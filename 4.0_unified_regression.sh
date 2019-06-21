4ebb06a5ef80cff1940e1656e3d94b08b4cb2f27
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2019-06-21 13:39:17 +0100

    Update 4.0_unified_regression.sh

--- 4.0_unified_regression.sh
+++ 4.0_unified_regression.sh
@@ -1,30 +1,34 @@
-echo "Processing data and running regressions..."
-
 echo "Partitioning genotype file"
-#Partition genotype file
+#Partition genotype file into chunks of 10,000 SNPs
+split -l10000 -d example.bim extract
 
-declare -a arr=()
+#Number of SNPs in .bim file
+snpnumber=$(wc -l < example.bim)
+
+#Rounding for truncation and count number of files
+round=$(echo "$((snpnumber+999))")
+partitions=$(echo "$((round/n))")
+
+echo "Running analysis"
+#Run analysis on partitioned files
+for i in $(seq 1 $partitions); 
+do 
+j=$(echo "$((i-1))")
+k=`printf "%02d" $j`
 
 #Convert to .raw
+plink \
+--bfile example \
+--extract extract${k} \
+--recodeA \
+--out temp.${i}
 
-for num in "${arr[@]}"; do 									
-	(
-  #Convert to .raw
-	plink \
-  --bfile ${a} \
-  --extract partition.{a} \
-  --recodeA \
-  --out temp.{a}.raw
-	
+#Run regression script in R
+Rscript resources/regression/unified_regression.R \
+temp.${i}
 
-	
-	Rscript resources/regression/unified_regression.R \
-        ${arg1} \
-        ${arg2} \
-        ${arg3}
-        
-rm temp.{a}.raw
-	) 
+#Remove .raw file
+rm temp.${i}*;
 done
 
-wait
+echo "Completed analysis"
