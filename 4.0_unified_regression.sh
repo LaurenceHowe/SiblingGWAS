dad35dd51ce78376b080de84e15d52693a8bfe3f
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2019-06-21 11:57:17 +0100

    Update 4.0_unified_regression.sh

--- 4.0_unified_regression.sh
+++ 4.0_unified_regression.sh
@@ -1,1 +1,30 @@
+echo "Processing data and running regressions..."
 
+echo "Partitioning genotype file"
+#Partition genotype file
+
+declare -a arr=()
+
+#Convert to .raw
+
+for num in "${arr[@]}"; do 									
+	(
+  #Convert to .raw
+	plink \
+  --bfile ${a} \
+  --extract partition.{a} \
+  --recodeA \
+  --out temp.{a}.raw
+	
+
+	
+	Rscript resources/regression/unified_regression.R \
+        ${arg1} \
+        ${arg2} \
+        ${arg3}
+        
+rm temp.{a}.raw
+	) 
+done
+
+wait
