2645dffb4c15db06d888f8c0d779c9bd8b55cdf0
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2019-06-26 10:36:08 +0100

    Create 3.0_partitions.sh

---
+++ 3.0_partitions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+source ./config
+
+exec &> >(tee ${section_01_logfile})
+
+echo "Partitioning genotype file"
+
+#Partition size
+size=${chunks_snp_number}
+
+#Partition genotype file into chunks of partition size SNPs
+split -l$size -d ${bfile.raw}.bim ${section_03_dir).extract
+
+#Number of SNPs in .bim file
+snpnumber=$(wc -l < ${bfile.raw}.bim)
+
+#Rounding for truncation and count number of files
+
+round=$(echo "$((snpnumber+$size-1))")
+partitions=$(echo "$((round/$size))")
