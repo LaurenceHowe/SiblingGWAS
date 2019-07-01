f87064f8b3b01cea115bfff352882078090203eb
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2019-07-01 11:44:40 +0100

    Update 5.0_tidy.sh

--- 5.0_tidy.sh
+++ 5.0_tidy.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+set -e
+source ./config
+
+mkdir -p ${section_04_dir}/merged
+
+## Sibling unified regression
+echo "Merging results across batches..."
+
+# Extract header from first results file
+head -n1 ${section_04_dir}/output.1_results.txt > ${section_04_dir}/merged/header.txt
+
+# Remove headers from all files
+sed -i '1d' output/split*_sibs_WFunified_results.txt
+
+# Merge all files
+cat ${section_04_dir}/merged/header.txt ${section_04_dir}/output* > ${section_04_dir}/merged/combined.sumstats.txt
+
+echo "All finished!"
