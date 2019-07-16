e292deacdc2e544093f94f89239498f28d411d8d
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2019-07-16 10:31:04 +0100

    Update 5.0_tidy.sh

--- 5.0_tidy.sh
+++ 5.0_tidy.sh
@@ -3,6 +3,8 @@
 set -e
 source ./config
 
+gwasoutcome=${1}
+
 mkdir -p ${section_05_dir}
 mkdir -p ${section_05_dir}/logs
 
@@ -10,12 +12,12 @@ mkdir -p ${section_05_dir}/logs
 echo "Merging results across batches..."
 
 # Extract header from first results file
-head -n1 ${section_04_dir}/output.1_results.txt > ${section_04_dir}/header.txt
+head -n1 ${section_04_dir}/output.1.${gwasoutcome}_results.txt > ${section_04_dir}/header.${gwasoutcome}.txt
 
 # Remove headers from all files
-sed -i '1d' output/split*_sibs_WFunified_results.txt
+sed -i '1d' ${section_04_dir}/output.*.${gwasoutcome}_results.txt
 
 # Merge all files
-cat ${section_04_dir}/header.txt ${section_04_dir}/output* > ${section_05_dir}/combined.sumstats.txt
+cat ${section_04_dir}/header.${gwasoutcome}.txt ${section_04_dir}/output.*.${gwasoutcome}_results.txt > ${section_05_dir}/combined.sumstats.txt
 
 echo "All finished!"
