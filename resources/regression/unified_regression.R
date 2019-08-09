b540e1eee013efc497732ad0cead570cb7499db8
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2019-08-09 11:37:30 +0100

    Update unified_regression.R

--- resources/regression/unified_regression.R
+++ resources/regression/unified_regression.R
@@ -44,7 +44,7 @@ output <- data.frame(CHR=bim$V1, SNP=bim$V2, BP=bim$V4, A1=bim$V5, A2=bim$V6, N_
                      SHRINK_BETA=NA, SHRINK_SE=NA)
 
 #---------------------------------------------------------------------------------------------#
-# loop over all SNPs in a chromosome (should take about 10 hrs)
+# Loop over all SNPs in the partition
 #---------------------------------------------------------------------------------------------#
 paste0("Looping...")
 ptm <- proc.time()
@@ -71,25 +71,34 @@ for (i in 1:length(Variants)) {
     model1 <-lm(formula = PHENOTYPE ~ GENOTYPE + AGE + SEX +PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10+PC11+PC12+PC13+PC14+PC15+PC16+PC17+PC18+PC19+PC20, data=ped3)
     
     #Save Beta information
+    output$BETA_MODEL1_0<-model1$coefficients[1]
     output$BETA_TOTAL[i]<-model1$coefficients[2]
     
     # save the variance covariance matrix to cluster SEs by family
-    vcv_matrix = vcovCL(model1, cluster=ped2$FID)
-    if(  is.na(output$BETA_TOTAL[i])) {
-        output$VCV_TOTAL[i] <-NA
+    vcv_matrix = vcovCL(model1, cluster=ped3$FID)
+    if(  is.na(output$BETA_MODEL1_0[i]) | is.na(output$BETA_TOTAL[i])) {
+        output$VCV_MODEL1_0[i] <-NA
+        output$VCV_MODEL1_0_TOTAL[i] <-NA
+        output$VCV_MODEL1_TOTAL[i] <-NA
     } else {
-        output$VCV_TOTAL[i] <- vcv_matrix[2,2]
+        output$VCV_MODEL1_0[i] <- vcv_matrix[1,1]
+        output$VCV_MODEL1_0_TOTAL[i] <- vcv_matrix[1,2]
+        output$VCV_MODEL1_TOTAL[i] <- vcv_matrix[2,2]
  
     }
 
     #Derive the clustered SEs for the total effect and P-values
-    test_matrix <- coeftest(fit, vcov.=vcv_matrix)
-    if(  is.na(output$BETA_TOTAL[i]) ) {
-        output$SE_TOTAL[i] <- NA
-        output$P_TOTAL[i] <- NA
+    test_matrix <- coeftest(model1, vcov.=vcv_matrix)
+    if(  is.na(output$BETA_MODEL1_0[i]) | is.na(output$BETA_TOTAL[i])) {
+        output$SE_BETA_MODEL1_0[i] <- NA
+        output$SE_BETA_TOTAL[i] <- NA
+        output$P_BETA_MODEL1_0[i] <- NA
+        output$P_BETA_TOTAL[i] <- NA
     } else {
-        output$SE_TOTAL[i] <- test_matrix[2,2] 
-        output$P_TOTAL[i] <- test_matrix[2,4] 
+        output$SE_BETA_MODEL1_0[i] <- test_matrix[1,2] 
+        output$SE_BETA_TOTAL[i] <- test_matrix[2,2]
+        output$P_BETA_MODEL1_0[i] <- test_matrix[1,4] 
+        output$P_BETA_TOTAL[i] <- test_matrix[2,4] 
  
     }
     
@@ -98,46 +107,46 @@ for (i in 1:length(Variants)) {
     model2 <- lm(formula = PHENOTYPE ~ FAM_MEAN + CENTREDGENOTYPE + AGE + SEX+PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10+PC11+PC12+PC13+PC14+PC15+PC16+PC17+PC18+PC19+PC20, data=ped3)
    
     # Sample size in regression
-    output$N_REG[i] <- length(resid(fit2))
+    output$N_REG[i] <- length(resid(model2))
     
     # Save Beta information
-    output$BETA_0[i] <- fit2$coefficients[1]
+    output$BETA_MODEL2_0[i] <- fit2$coefficients[1]
     output$BETA_BF[i] <- fit2$coefficients[2]
     output$BETA_WF[i] <- fit2$coefficients[3]
     
     
     # save the variance covariance matrix
-    vcv_matrix = vcovCL(fit2, cluster=ped3$FID)
-    if(  is.na(output$BETA_0[i]) | is.na(output$BETA_BF[i]) | is.na(output$BETA_WF[i]) ) {
-        output$VCV_0[i] <-NA
-        output$VCV_0_BF[i] <-NA
-        output$VCV_0_WF[i] <-NA
-        output$VCV_BF[i] <-NA
-        output$VCV_BF_WF[i] <-NA
-        output$VCV_WF[i] <-NA
+    vcv_matrix = vcovCL(model2, cluster=ped3$FID)
+    if(  is.na(output$BETA_MODEL2_0[i]) | is.na(output$BETA_BF[i]) | is.na(output$BETA_WF[i]) ) {
+        output$VCV_MODEL2_0[i] <-NA
+        output$VCV_MODEL2_0_BF[i] <-NA
+        output$VCV_MODEL2_0_WF[i] <-NA
+        output$VCV_MODEL2_BF[i] <-NA
+        output$VCV_MODEL2_BF_WF[i] <-NA
+        output$VCV_MODEL2_WF[i] <-NA
     } else {
-        output$VCV_0[i] <- vcv_matrix[1,1]
-        output$VCV_0_BF[i] <- vcv_matrix[1,2]
-        output$VCV_0_WF[i] <- vcv_matrix[1,3]
-        output$VCV_BF[i] <- vcv_matrix[2,2]
-        output$VCV_BF_WF[i] <- vcv_matrix[2,3]
-        output$VCV_WF[i] <- vcv_matrix[3,3]
+        output$VCV_MODEL2_0[i] <- vcv_matrix[1,1]
+        output$VCV_MODEL2_0_BF[i] <- vcv_matrix[1,2]
+        output$VCV_MODEL2_0_WF[i] <- vcv_matrix[1,3]
+        output$VCV_MODEL2_BF[i] <- vcv_matrix[2,2]
+        output$VCV_MODEL2_BF_WF[i] <- vcv_matrix[2,3]
+        output$VCV_MODEL2_WF[i] <- vcv_matrix[3,3]
     }
 
     # save the clustered SEs and corresponding P-values for WF/BF
-    test_matrix <- coeftest(fit2, vcov.=vcv_matrix)
+    test_matrix <- coeftest(model2, vcov.=vcv_matrix)
     if(  is.na(output$BETA_0[i]) | is.na(output$BETA_BF[i]) | is.na(output$BETA_WF[i]) ) {
-        output$SE_BETA_0[i] <- NA
+        output$SE_BETA_MODEL2_0[i] <- NA
         output$SE_BETA_BF[i] <- NA
         output$SE_BETA_WF[i] <- NA
-        output$P_BETA_0[i] <- NA
+        output$P_BETA_MODEL2_0[i] <- NA
         output$P_BETA_BF[i] <- NA
         output$P_BETA_WF[i] <- NA
     } else {
-        output$SE_BETA_0[i] <- test_matrix[1,2] 
+        output$SE_BETA_MODEL2_0[i] <- test_matrix[1,2] 
         output$SE_BETA_BF[i] <- test_matrix[2,2] 
         output$SE_BETA_WF[i] <- test_matrix[3,2] 
-        output$P_BETA_0[i] <- test_matrix[1,4] 
+        output$P_BETA_MODEL2_0[i] <- test_matrix[1,4] 
         output$P_BETA_BF[i] <- test_matrix[2,4] 
         output$P_BETA_WF[i] <- test_matrix[3,4] 
     }
