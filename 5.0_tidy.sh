aa0507d9c3d5cdd669d701751291af1a2cf1fc47
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2019-09-04 15:30:50 +0100

    Update 5.0_tidy.sh

--- 5.0_tidy.sh
+++ 5.0_tidy.sh
@@ -18,6 +18,6 @@ head -n1 ${section_04_dir}/output.1.${gwasoutcome}_results.txt > ${section_04_di
 sed -i '1d' ${section_04_dir}/output.*.${gwasoutcome}_results.txt
 
 # Merge all files
-cat ${section_04_dir}/header.${gwasoutcome}.txt ${section_04_dir}/output.*.${gwasoutcome}_results.txt > ${section_05_dir}/combined.sumstats.${gwasoutcome}.txt
+cat ${section_04_dir}/header.${gwasoutcome}.txt ${section_04_dir}/output.*.${gwasoutcome}_results.txt > ${section_05_dir}/${study_name}.sumstats.${gwasoutcome}.txt
 
 echo "All finished!"
