f091c4639fd5fa755caaaab16b60b3e000223c22
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2019-09-09 10:59:18 +0100

    Update 5.0_tidy.sh

--- 5.0_tidy.sh
+++ 5.0_tidy.sh
@@ -15,9 +15,12 @@ echo "Merging results across batches..."
 head -n1 ${section_04_dir}/output.1.${gwasoutcome}_results.txt > ${section_04_dir}/header.${gwasoutcome}.txt
 
 # Remove headers from all files
-sed -i '1d' ${section_04_dir}/output.*.${gwasoutcome}_results.txt
+sed -i '1d' ${section_04_dir}/output.*.${gwasoutcome}_results.txt > temp.
 
-# Merge all files
-cat ${section_04_dir}/header.${gwasoutcome}.txt ${section_04_dir}/output.*.${gwasoutcome}_results.txt > ${section_05_dir}/${study_name}.sumstats.${gwasoutcome}.txt
+# Merge all files using a temporary file in case there's an error
+cat ${section_04_dir}/header.${gwasoutcome}.txt ${section_04_dir}/output.*.${gwasoutcome}_results.txt > ${section_05_dir}/temporary_file.txt
+
+#Create a temporary file incase errors with the renaming
+mv ${section_05_dir}/temporary_file.txt ${section_05_dir}/${study_name}.sumstats.${gwasoutcome}.txt
 
 echo "All finished!"
