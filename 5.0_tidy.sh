00abd591b8a9e5f1f0eb3a8ca5a662a09e9e4ed4
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2019-09-12 13:12:27 +0100

    Update 5.0_tidy.sh

--- 5.0_tidy.sh
+++ 5.0_tidy.sh
@@ -23,10 +23,10 @@ cat ${section_04_dir}/header.${gwasoutcome}.txt ${section_04_dir}/output.*.${gwa
 #Finalise file
 mv ${section_05_dir}/temporary_file.txt ${section_05_dir}/${study_name}.sumstats.${gwasoutcome}.txt
 
-echo "Combined summary statistics file created!"
+echo "Combined summary statistics file created!, now gzipping..."
+gzip ${section_05_dir}/${study_name}.sumstats.${gwasoutcome}.txt
 
-echo "Removing batch results files in SiblingGWAS/results/04"...
-
+echo "Removing batch results files in SiblingGWAS/results/04 ..."
 rm ${section_04_dir}/output.*.${gwasoutcome}_results.txt
 
 echo "All finished!"
