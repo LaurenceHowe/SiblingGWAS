0c9e6d3b9f79ed4d3c6ce98f5f2078ca9a2df545
BenBrumpton <bmbrumpton@gmail.com>
2019-09-24 07:25:05 +0000

    added script to install R packages

--- misc/install_r_packages.sh
+++ misc/install_r_packages.sh
@@ -4,4 +4,6 @@ sudo su - -c "R -e \"install.packages('sandwich', repos='http://cran.rstudio.com
 sudo su - -c "R -e \"install.packages('data.table', repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('plyr', repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('matrixStats', repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"install.packages('nlWaldTest', repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"install.packages('inline', repos='http://cran.rstudio.com/')\""
 
