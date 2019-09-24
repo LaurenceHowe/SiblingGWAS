a914e577de38fda86b7da8a2916ff50f3dad49c2
BenBrumpton <bmbrumpton@gmail.com>
2019-09-24 07:21:41 +0000

    added maunual value for batches

--- 4.1_parallel_uni_reg.sh
+++ 4.1_parallel_uni_reg.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# This script runs 4.0_unified_regression.sh in a for loop for all batches and traits
+# This script runs 4.0_unified_regression.sh in parallel for all batches and traits
 
 # Number of batches
-n_batches=`ls ~/SiblingGWAS/results/03 | grep 'extract' | wc -l`
+
+#n_batches=`ls ~/SiblingGWAS/results/03 | grep 'extract' | wc -l`
+n_batches=10
 
 # Name of trait
+
 trait=BMI
 
-# Loop through batches
+# Run with j threads
 
-parallel -j30 ~/SiblingGWAS/4.0_unified_regression.sh {1} $trait ::: $(seq 0 $n_batches)
+parallel -j 11 --joblog out3.log ~/SiblingGWAS/4.0_unified_regression.sh {1} $trait ::: $(seq 0 $n_batches)
 
