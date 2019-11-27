6e0ceccd9e602c737994e0b617fad36efb6ed549
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2019-11-27 10:44:28 +0000

    Fixed errors.

--- resources/siblings/checks.R
+++ resources/siblings/checks.R
@@ -17,7 +17,8 @@ relMZ<-rel[which(rel$PI_HAT>0.98),]
 MZ<-nrow(relMZ)
 message("Number of monozygotic twin-pairs in sample: ", MZ)
 
-for (i in 1:length(relMZ)) {
+if (MZ > 0) {
+for (i in 1:nrow(relMZ)) {
   data<-rel[which(rel$FID1==relMZ$FID1[i] | rel$FID2==relMZ$FID1[i]),]
 if(nrow(data)<2)
 	{
@@ -27,6 +28,7 @@ if(nrow(data)<2)
 	warning("ERROR: ", msg)
 	}
 }
+}
 
 #Check Parents
 
@@ -34,7 +36,7 @@ relPARENTS<-rel[which(rel$Z1>0.98),]
 
 if(nrow(relPARENTS)>0)
 	{
-	for (i in 1:length(relPARENTS)) {
+	for (i in 1:nrow(relPARENTS)) {
 	msg <-paste0("Identified Parent-offspring pairs: Please check or remove the following families: IDs ", relPARENTS[i,1])
 	errorlist<-c(errorlist, msg)
 	warning("ERROR: ", msg)
@@ -45,7 +47,7 @@ if(nrow(relPARENTS)>0)
 relLOW<-rel[which(rel$PI_HAT<0.3),]
 if(nrow(relLOW)>0)
 	{
-	for (i in 1:length(relPARENTS)) {
+	for (i in 1:nrow(relPARENTS)) {
 	msg <-paste0("Identified pair in same family with IBD estimate <0.3: Please check or remove the following families: IDs ", relLOW[i,1])
 	errorlist<-c(errorlist, msg)
 	warning("ERROR: ", msg)
