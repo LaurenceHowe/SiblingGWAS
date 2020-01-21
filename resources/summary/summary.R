e5ca8494a6a5221941be969a10aef00a5f142e49
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2020-01-21 15:07:11 +0000

    Update summary.R

--- resources/summary/summary.R
+++ resources/summary/summary.R
@@ -23,9 +23,9 @@ phen<-paste(plist[i])
 ph2<-subset(ph, select=c("IID", phen))
 names(ph2)<-c("IID", "Outcome")
 
-mean<-mean(ph2$Outcome, na.rm=T)
-sd<-sd(ph2$Outcome, na.rm=T)
-median<-median(ph2$Outcome, na.rm=T)
+mean <- mean(ph2$Outcome, na.rm=T)
+sd <- sd(ph2$Outcome, na.rm=T)
+median <- median(ph2$Outcome, na.rm=T)
 
 q1 <- as.numeric(summary(ph2$Outcome)[2])
 q3 <- as.numeric(summary(ph2$Outcome)[5])
@@ -35,19 +35,25 @@ max <- as.numeric(summary(ph2$Outcome)[6])
 miss <- is.na(ph2$Outcome)
 N <- as.numeric(summary(miss)[2])
 
-stats<-data.frame(phen, N, mean, sd, median, min, max, q1, q3)
+model1 <- lm(Outcome ~ Sex + Age + PC1 + PC2 + PC3 + PC4 + PC5
+             + PC6 + PC7 + PC8 + PC9 + PC10
+             + PC11 + PC12 + PC13 + PC14 + PC15
+             + PC16 + PC17 + PC18 + PC19 + PC20, data = ph2)
+sd_rsid <- sd(resid(model1))
+  
+stats<-data.frame(phen, N, mean, sd, sd_resid, median, min, max, q1, q3)
 output<-rbind(output,stats)
 }
 
 
 for (i in 1:nrow(clist)) {
-phen<-paste(clist[i])
-cov2<-subset(cov, select=c("IID", phen))
-names(cov2)<-c("IID", "Covariate")
+phen <- paste(clist[i])
+cov2 <- subset(cov, select = c("IID", phen))
+names(cov2) <- c("IID", "Covariate")
 
-mean<-mean(cov2$Covariate, na.rm=T)
-sd<-sd(cov2$Covariate, na.rm=T)
-median<-median(cov2$Covariate, na.rm=T)
+mean <- mean(cov2$Covariate, na.rm = T)
+sd <- sd(cov2$Covariate, na.rm = T)
+median <- median(cov2$Covariate, na.rm = T)
 
 q1 <- as.numeric(summary(cov2$Covariate)[2])
 q3 <- as.numeric(summary(cov2$Covariate)[5])
@@ -57,7 +63,9 @@ max <- as.numeric(summary(cov2$Covariate)[6])
 miss <- is.na(cov2$Covariate)
 N <- as.numeric(summary(miss)[2])
 
-stats<-data.frame(phen, N, mean, sd, median, min, max, q1, q3)
+sd_resid <- NA
+  
+stats<-data.frame(phen, N, mean, sd, sd_resid, median, min, max, q1, q3)
 output<-rbind(output,stats)
 }
 
