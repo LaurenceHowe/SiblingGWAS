5a0bff1b7bbfa6f41e32c1a4c13c3c07160e494e
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2020-01-21 15:12:03 +0000

    Update summary.R

--- resources/summary/summary.R
+++ resources/summary/summary.R
@@ -49,7 +49,7 @@ model1 <- lm(Outcome ~ Sex + Age + PC1 + PC2 + PC3 + PC4 + PC5
              + PC6 + PC7 + PC8 + PC9 + PC10
              + PC11 + PC12 + PC13 + PC14 + PC15
              + PC16 + PC17 + PC18 + PC19 + PC20, data = ph2)
-sd_rsid <- sd(resid(model1))
+sd_resid <- sd(resid(model1))
   
 stats<-data.frame(phen, N, mean, sd, sd_resid, median, min, max, q1, q3)
 output<-rbind(output,stats)
